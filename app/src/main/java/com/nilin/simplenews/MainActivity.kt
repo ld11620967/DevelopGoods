d74e4ee2c11677ffda03cbf8db2e37b25fef6721
nilin <11620967@qq.com>
2017-06-23 16:17:19 +0800

    updata

--- app/src/main/java/com/nilin/simplenews/MainActivity.kt
+++ app/src/main/java/com/nilin/simplenews/MainActivity.kt
@@ -1,3 +1,5 @@
+@file:Suppress("DEPRECATION")
+
 package com.nilin.simplenews
 
 import android.content.Context
@@ -9,9 +11,7 @@ import android.util.Log
 import com.cjj.MaterialRefreshLayout
 import com.cjj.MaterialRefreshListener
 import com.google.gson.Gson
-import com.nilin.simplenews.R.styleable.MaterialRefreshLayout
 import com.nilin.simplenews.model.ApiGank
-import com.nilin.simplenews.model.NewsModel
 import kotlinx.android.synthetic.main.activity_main.*
 import org.jetbrains.anko.doAsync
 import org.jetbrains.anko.sdk25.coroutines.onClick
@@ -20,60 +20,57 @@ import org.jetbrains.anko.uiThread
 import java.net.URL
 
 
-@Suppress("DEPRECATION")
-abstract class MainActivity : AppCompatActivity() {
-    var mList = ArrayList<NewsModel>()
-    lateinit var mAdapter: NewsAdapter
+class MainActivity : AppCompatActivity() {
+    //    var mList = ArrayList<NewsModel>()
+//    lateinit var mAdapter: NewsAdapter
+//    lateinit var refreshLayout: MaterialRefreshLayout
 
-    var refreshLayout: MaterialRefreshLayout = null!!
+        override fun onCreate(savedInstanceState: Bundle?) {
+            super.onCreate(savedInstanceState)
+            setContentView(R.layout.activity_main)
 
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
-        val toolbar = findViewById(R.id.toolbar) as Toolbar
-        setSupportActionBar(toolbar)
+            val toolbar = findViewById(R.id.toolbar) as Toolbar
+            setSupportActionBar(toolbar)
 
 
-        var url = "http://gank.io/api/data/Android/10/1"
+            var url = "http://gank.io/api/data/Android/10/1"
 
-        var context: Context = this
-        doAsync {
-            var returnjsonstr = URL(url).readText()
-            var returnjson = Gson().fromJson(returnjsonstr, ApiGank::class.java)
-            Log.i("haha", "haha" + returnjson.results)
-            uiThread {
-                recyclerview.layoutManager = GridLayoutManager(context, 1)
-                recyclerview.adapter = NewsAdapter(returnjson.results)
-            }
-        }
-
-        fab.onClick {
-            toast("haha")
-        }
-
-        refreshLayout.setLoadMore(true)
-        refreshLayout = findViewById(R.id.refresh) as MaterialRefreshLayout
-        refreshLayout.setMaterialRefreshListener(MaterialRefreshListener() {
-
-            override fun onRefresh(refreshLayout: MaterialRefreshLayout) {
-                //下拉刷新...
+            var context: Context = this
+            doAsync {
+                var returnjsonstr = URL(url).readText()
+                var returnjson = Gson().fromJson(returnjsonstr, ApiGank::class.java)
+                Log.i("haha", "haha" + returnjson.results)
+                uiThread {
+                    recyclerview.layoutManager = GridLayoutManager(context, 1)
+                    recyclerview.adapter = NewsAdapter(returnjson.results)
+                }
             }
 
-
-            override fun onRefreshLoadMore(refreshLayout: MaterialRefreshLayout) {
-                //上拉刷新...
+            fab.onClick {
+                toast("haha")
             }
-        }
-        )
+
+//            refreshLayout.setLoadMore(true)
+//            refreshLayout = findViewById(R.id.refresh) as MaterialRefreshLayout
+//            refreshLayout.setMaterialRefreshListener(object : MaterialRefreshListener() {
+
+            refresh.setMaterialRefreshListener(object : MaterialRefreshListener() {
+
+                override fun onRefresh(refreshLayout: MaterialRefreshLayout) {
+                    toast("shuaxin")
+                    refresh.finishRefresh();
+                }
+//                refresh.setLoadMore(true);
+                override fun onRefreshLoadMore(refreshLayout: MaterialRefreshLayout) {
+                    //上拉刷新...
+                    toast("jiazai")
+                    refresh.finishRefreshLoadMore();
+                }
+            }
+            )
 
 
-//// 结束下拉刷新...
-//    materialRefreshLayout.finishRefresh();
-//
-//// 结束上拉刷新...
-//    materialRefreshLayout.finishRefreshLoadMore();
+
 
 
 //        mAdapter = NewsAdapter(mList)
@@ -90,7 +87,6 @@ abstract class MainActivity : AppCompatActivity() {
 //        context.startActivity(intent)
 
     }
-
 }
 
 
