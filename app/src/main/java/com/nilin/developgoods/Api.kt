7f440d52caee2099bd811abf9c863a191cdb6b6f
nilin <11620967@qq.com>
2017-07-11 21:04:51 +0800

    updata

---
+++ app/src/main/java/com/nilin/developgoods/Api.kt
@@ -0,0 +1,46 @@
+package com.nilin.retrofit2_rxjava2_demo
+
+import com.nilin.developgoods.model.ApiGank
+import com.nilin.developgoods.model.NewsModel
+import io.reactivex.Observable
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.http.GET
+import retrofit2.http.Path
+
+
+/**
+ * Created by liangd on 2017/7/11.
+ */
+interface Api {
+
+    @GET("api/data/{type}/{pageSize}/{pageNumber}")
+    fun getData(@Path("type") type: String,
+                @Path("pageSize") pageSize: Int,
+                @Path("pageNumber") pageNumber: Int): Observable<ApiGank>
+
+
+
+    companion object Factory{
+        fun create():Api {
+
+            val logging = HttpLoggingInterceptor()
+            logging.level = HttpLoggingInterceptor.Level.BASIC
+            val client = OkHttpClient.Builder()
+                    .addInterceptor(logging)
+                    .build()
+
+            val retrofit = Retrofit.Builder()
+                    .client(client)
+                    .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .baseUrl("http://gank.io/")
+                    .build()
+
+            return retrofit.create(Api::class.java)
+        }
+    }
+}
--- app/src/main/java/com/nilin/developgoods/MainActivity.kt
+++ app/src/main/java/com/nilin/developgoods/MainActivity.kt
@@ -3,6 +3,7 @@ package com.nilin.developgoods
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.widget.GridLayoutManager
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.Toolbar
 import android.util.Log
@@ -15,16 +16,19 @@ import org.jetbrains.anko.toast
 import java.net.URL
 import com.jcodecraeer.xrecyclerview.XRecyclerView
 import com.nilin.developgoods.model.NewsModel
+import com.nilin.retrofit2_rxjava2_demo.Api
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.schedulers.Schedulers
 import org.jetbrains.anko.doAsync
 import org.jetbrains.anko.uiThread
 import kotlin.collections.ArrayList
 
 class MainActivity : AppCompatActivity() {
+    var published:String? = null
+    var adapter: NewsAdapter? = null
     var context = this
     private var mList = ArrayList<NewsModel>()
-    //    lateinit var mAdapter:NewsAdapter
-    var mAdapter = NewsAdapter(mList)
-    var number = 10
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -33,65 +37,62 @@ class MainActivity : AppCompatActivity() {
         val toolbar = findViewById(R.id.toolbar) as Toolbar
         setSupportActionBar(toolbar)
 
+        loadPublishedDate()
 
-        var url = "http://gank.io/api/data/Android/10/1"
-        doAsync {
-            var returnjsonstr = URL(url).readText()
-            var returnjson = Gson().fromJson(returnjsonstr, ApiGank::class.java)
 
-            mList.addAll(returnjson.results)
+//        var url = "http://gank.io/api/data/Android/10/1"
+//        doAsync {
+//            var returnjsonstr = URL(url).readText()
+//            var returnjson = Gson().fromJson(returnjsonstr, ApiGank::class.java)
 
-            var mAdapter = NewsAdapter(mList)
-            uiThread {
+//            mList.addAll(returnjson.results)
+
+
+//            mAdapter!!.setNewData(returnjson.results)
+//            uiThread {
                 //recyclerview.layoutManager = GridLayoutManager(context, 1)
-                recyclerview.setLayoutManager(LinearLayoutManager(context))
-                recyclerview.setAdapter(mAdapter)
-
-                mAdapter.setOnItemClickListener(object : NewsAdapter.OnItemClickListener {
-                    override fun onItemClick(view: View, position: Int) {
-                        val intent = Intent()
-                        intent.setClass(context, DetailsActivity::class.java)
-                        intent.putExtra("url", returnjson.results.get(position).url)
-                        context.startActivity(intent)
-                    }
-                })
-            }
-        }
+//                recyclerview.setLayoutManager(LinearLayoutManager(context))
+//                recyclerview.setAdapter(mAdapter)
+//                mAdapter!!.setNewData(returnjson.results)
+//                mAdapter.setOnItemClickListener(object : NewsAdapter.OnItemClickListener {
+//                    override fun onItemClick(view: View, position: Int) {
+//                        val intent = Intent()
+//                        intent.setClass(context, DetailsActivity::class.java)
+//                        intent.putExtra("url", returnjson.results.get(position).url)
+//                        context.startActivity(intent)
+//                    }
+//                })
+//            }
+//        }
 //        updata(url)
 
-        fab.onClick {
-            recyclerview.smoothScrollToPosition(0)
-            toast("已达顶部")
-        }
+//        fab.onClick {
+//            recyclerview.smoothScrollToPosition(0)
+//            toast("已达顶部")
+//        }
 
-        recyclerview.setLoadingListener(object : XRecyclerView.LoadingListener {
-            override fun onRefresh() {
-//                mList.clear()
-                var url = "http://gank.io/api/data/Android/10/1"
-
-                updata(url)
-                toast("刷新成功")
-                recyclerview.refreshComplete()
-            }
-
-            override fun onLoadMore() {
-                number = number + 10
-                url = "http://gank.io/api/data/Android/$number/1"
-
-
-
-                doAsync {
-                    var returnjsonstr = URL(url).readText()
-
-                    var returnjson = Gson().fromJson(returnjsonstr, ApiGank::class.java)
-
-
-                    mList.addAll(returnjson.results)
-
-                    uiThread {
-                        mAdapter.notifyDataSetChanged()
-                    }
-                }
+//        recyclerview.setLoadingListener(object : XRecyclerView.LoadingListener {
+//            override fun onRefresh() {
+////                mList.clear()
+//                var url = "http://gank.io/api/data/Android/10/1"
+//
+//                updata(url)
+//                toast("刷新成功")
+//                recyclerview.refreshComplete()
+//            }
+//
+//            override fun onLoadMore() {
+//                number = number + 10
+//                url = "http://gank.io/api/data/Android/$number/1"
+//
+//                doAsync {
+//                    var returnjsonstr = URL(url).readText()
+//                    var returnjson = Gson().fromJson(returnjsonstr, ApiGank::class.java)
+//                    mList.addAll(returnjson.results)
+//                    uiThread {
+//                        mAdapter.notifyDataSetChanged()
+//                    }
+//                }
 
 //                doAsync {
 //                    var returnjsonstr = URL(url).readText()
@@ -108,36 +109,64 @@ class MainActivity : AppCompatActivity() {
 ////
 //                    }
 //                }
-                recyclerview.loadMoreComplete()
-            }
-        })
+//                recyclerview.loadMoreComplete()
+//            }
+//        })
     }
 
-    fun updata(url: String) {
-        doAsync {
-            var returnjsonstr = URL(url).readText()
-            var returnjson = Gson().fromJson(returnjsonstr, ApiGank::class.java)
-//            var mAdapter = NewsAdapter(context,returnjson.results)
-            mList.addAll(returnjson.results)
-            var mAdapter = NewsAdapter(mList)
-//            val list = ArrayList(asList(returnjson.results))
-//            var mAdapter = NewsAdapter(context,list)
-            uiThread {
-                //recyclerview.layoutManager = GridLayoutManager(context, 1)
-                recyclerview.setLayoutManager(LinearLayoutManager(context))
-                recyclerview.setAdapter(mAdapter)
 
-                mAdapter.setOnItemClickListener(object : NewsAdapter.OnItemClickListener {
-                    override fun onItemClick(view: View, position: Int) {
-                        val intent = Intent()
-                        intent.setClass(context, DetailsActivity::class.java)
-                        intent.putExtra("url", returnjson.results.get(position).url)
-                        context.startActivity(intent)
-                    }
-                })
 
-            }
-        }
+
+//    fun updata(url: String) {
+//        doAsync {
+//            var returnjsonstr = URL(url).readText()
+//            var returnjson = Gson().fromJson(returnjsonstr, ApiGank::class.java)
+////            var mAdapter = NewsAdapter(context,returnjson.results)
+//            mList.addAll(returnjson.results)
+//            var mAdapter = NewsAdapter(mList)
+////            val list = ArrayList(asList(returnjson.results))
+////            var mAdapter = NewsAdapter(context,list)
+//            uiThread {
+//                //recyclerview.layoutManager = GridLayoutManager(context, 1)
+////                recyclerview.setLayoutManager(LinearLayoutManager(context))
+////                recyclerview.setAdapter(mAdapter)
+//
+//                mAdapter.setOnItemClickListener(object : NewsAdapter.OnItemClickListener {
+//                    override fun onItemClick(view: View, position: Int) {
+//                        val intent = Intent()
+//                        intent.setClass(context, DetailsActivity::class.java)
+//                        intent.putExtra("url", returnjson.results.get(position).url)
+//                        context.startActivity(intent)
+//                    }
+//                })
+//
+//            }
+//        }
+//    }
+
+
+    fun loadPublishedDate() {
+
+        val api = Api.Factory.create()
+        api.getData("Android",10,1)
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribeOn(Schedulers.io())
+                .subscribe({
+                    result ->
+//                    published = result.results.toString()
+                    var mAdapter = NewsAdapter(result.results)
+//                    published = result.results[0].replace("-","/")
+                    Log.i("111",published)
+                    adapter.
+
+
+//                        published = result.results[0].replace("-","/")
+//                        parseResult(result)
+                }, {}, {  })
+//                    }, {}, { onComplete() })
+
+
     }
+
 }
 
